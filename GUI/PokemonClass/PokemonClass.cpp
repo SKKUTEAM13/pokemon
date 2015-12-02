9026493deef8bbd2ec6254b4ea381ef34a32e25c
jo4965 <zenike@naver.com>
2015-12-02 17:05:54 +0900

    포켓몬 종류 대거추가
    밸런스 조정 필요

--- GUI/PokemonClass/PokemonClass.cpp
+++ GUI/PokemonClass/PokemonClass.cpp
@@ -1,17 +1,16 @@
 #include <ctime>
 #include <iostream>
 #include "PokemonClass.h"
-#include <string>
 
 using namespace std;
 ///////////기본 포켓몬 클래스 함수 정의 
 
-Pokemon::Pokemon(int vital, int attack, int defense, int fullVital, int level, string name, string skillName1, string skillName2)
-	: vital(vital + level * 5), attack(attack + level), defense(defense + level), curExp(0), level(level)
+Pokemon::Pokemon(int vital, int attack, int defense, int fullVital, int level, char * name, char * skillName1, char * skillName2)
+	: vital(vital + level * 5), attack(attack + level), fullVital(vital + level * 5), defense(defense + level), curExp(0), level(level)
 {
-	this->name = name;
-	this->skillSeries[0] = skillName1;
-	this->skillSeries[1] = skillName2;
+	strcpy(this->name, name);
+	strcpy(this->skillSeries[0], skillName1);
+	strcpy(this->skillSeries[1], skillName2);
 	addExp = 10 + 2 * level;
 	limitExp = 10 * level;
 }
@@ -40,54 +39,90 @@ void Pokemon::Damaged(int damage) {
 	if (vital < 0)
 		vital = 0;
 }
+void Pokemon::Heal() {
+	vital = fullVital;
+}
+
+int Pokemon::getCurExp() { return curExp; }
+int Pokemon::getLimitExp() { return limitExp; }
+int Pokemon::getLevel() { return level; }
 int Pokemon::getAttack() { return attack; }
 int Pokemon::getVital() { return vital; }
 int Pokemon::getDefense() { return defense; }
-string Pokemon::getSkill(int i) { return skillSeries[i - 1]; } // 포켓몬이 가지고 있는 i 번째 스킬 이름 반환. 
-string Pokemon::getName() { return name; }
+int Pokemon::getFullVital() { return fullVital; }
+
+char * Pokemon::getSkill(int i) { return skillSeries[i - 1]; } // 포켓몬이 가지고 있는 i 번째 스킬 이름 반환. 
+char * Pokemon::getName() { return name; }
 
 
+int Pokemon::FirstSkill(char skillName[])  // 첫번째 스킬 기본 데미지와 이름 반환
+{
+	srand(time(NULL));
+	int variable = rand() % 15 - 5;
+	strcpy(skillName, getSkill(1));
+	int damage = getAttack() + variable;
+	return damage;
+}
+
+
+int Pokemon::SecondSkill(char skillName[]) // 두번째 스킬 기본 데미지와 이름 반환
+{
+	srand(time(NULL));
+	int variable = rand() % 10 - 3;
+	strcpy(skillName, getSkill(2));
+	int damage = getAttack() + variable;
+	return damage;
+}
 
 ////// 피카츄 클래스 함수 정의
-Pikachu::Pikachu(int level) : Pokemon(70, 45, 35, 70, level, "피카츄", "백만 볼트", "몸통 박치기") { }
+Pikachu::Pikachu(int level) : Pokemon(70, 45, 35, 70, level, "pikachu", "Thunder bolt", "Iron Tail") { }
 
-int Pikachu::FirstSkill(string skillName)  // 첫번째 스킬 기본 데미지와 이름 반환
-{
-	srand(time(NULL));
-	int variable = rand() % 15 - 5;
-	skillName = getSkill(1);
-	int damage = getAttack() + variable;
-	return damage;
-}
-
-int Pikachu::SecondSkill(string skillName) // 두번째 스킬 기본 데미지와 이름 반환
-{
-	srand(time(NULL));
-	int variable = rand() % 10 - 3;
-	skillName = getSkill(2);
-	int damage = getAttack() + variable;
-	return damage;
-}
 
 ///// 구구 클래스 함수 정의
+GuGu::GuGu(int level) : Pokemon(60, 50, 30, 60, level, "Arboke", "Bite", "First attack") { }
 
-GuGu::GuGu(int level) : Pokemon(60, 50, 30, 60, level, "구구", "바람일으키기", "전광석화") { }
 
-int GuGu::FirstSkill(string skillName)
-{
-	srand(time(NULL));
-	int variable = rand() % 15 - 5;
-	skillName = getSkill(1);
-	int damage = getAttack();
-	return damage;
-}
+Aboke::Aboke(int level) : Pokemon(70, 50, 20, 70, level, "Arboke", "Poison Bite", "Poison Tail") { }
 
-int GuGu::SecondSkill(string skillName)
-{
-	srand(time(NULL));
-	int variable = rand() % 10 - 3;
-	skillName = getSkill(2);
-	int damage = getAttack() + 10;
-	return damage;
-}
+NyaongE::NyaongE(int level) : Pokemon(45, 55, 25, 45, level, "Naong", "Cat Money", "Claw") { }
+
+
+Ddogas::Ddogas(int level) : Pokemon(70, 50, 30, 70, level, "Ddogas", "Poison Gas", "Poison Dust") { }
+
+Ddodogas::Ddodogas(int level) : Pokemon(110, 60, 40, 110, level, "Ddodogas", "Venom", "Smog") { }
+
+JilpeokE::JilpeokE(int level) : Pokemon(90, 35, 30, 90, level, "JilpeokE", "Sewage Wave", "Belch") { }
+
+
+Coil::Coil(int level) : Pokemon(45, 50, 25, 45, level, "Coil", "Wild Volt", "Spark") { }
+
+
+Digda::Digda(int level) : Pokemon(50, 40, 35, 50, level, "Digda", "Dig Hole", "Mud Attack") { }
+
+Dactrio::Dactrio(int level) : Pokemon(70, 55, 40, 70, level, "Dactrio", "Earth Quake", "Sand Hole") { }
+
+Tanguri::Tanguri(int level) : Pokemon(60, 55, 25, 60, level, "Tanguri", "Bone Rush", "Bone Boomerang") { }
+
+
+Phantom::Phantom(int level) : Pokemon(80, 80, 20, 80, level, "Phantom", "Nightmare", "Shadow Punch") { }
+
+
+Yungela::Yungela(int level) : Pokemon(60, 70, 35, 60, level, "Yungela", "Psychic", "Psycho Cutter") { }
+
+
+Parasect::Parasect(int level) : Pokemon(70, 50, 30, 70, level, "Parasect", "Mushroom Spore", "Leaf Attack") { }
+
+
+Dandangi::Dandangi(int level) : Pokemon(40, 30, 55, 40, level, "Dandangi", "Bite", "Slap") { }
+
+
+Majayong::Majayong(int level) : Pokemon(60, 40, 40, 60, level, "Majayong", "Counter", "Mirror Coat") { }
+
+
+
+Yadon::Yadon(int level) : Pokemon(60, 30, 30, 60, level, "Yadon", "Water Beam", "Aqua Tail") { }
+
+Yadoran::Yadoran(int level) : Pokemon(80, 60, 40, 80, level, "Yadoran", "Shell Attack", "Hydropump") { }
+
+
 
--- GUI/PokemonClass/PokemonClass.h
+++ GUI/PokemonClass/PokemonClass.h
@@ -1,8 +1,6 @@
 #ifndef __POKEMONCLASS_H__
 #define __POKEMONCLASS_H__
 
-using namespace std;
-#include <string>
 
 class Pokemon
 {
@@ -15,39 +13,140 @@ private:
 	int curExp;				    // 현재 경험치
 	int addExp;				    // 이겼을 때 경험치 증가량
 	int limitExp;				// 현재 레벨 레벨업 경험치 필요량;
-	string name;
-	string skillSeries[4];
+	char name[20];
+	char skillSeries[4][30] = { 0, };
 public:
-	Pokemon(int vital, int attack, int defense, int fullVital, int level, string name, string skillName1, string skillName2);
-	virtual int FirstSkill(string skillName) = 0;
-	virtual int SecondSkill(string skillName) = 0;
+	Pokemon(int vital, int attack, int defense, int fullVital, int level, char * name, char * skillName1, char * skillName2);
 	void levelUp(int overExp);						// 초과한 경험치
 	int damageCalc(int skill, int opDefense);		// 나의 방어력과 속성등을 고려하여 상대방 공격의 데미지 계산
 	void Damaged(int damage);
+	void Heal();
+	int getCurExp();
+	int getLimitExp();
+	int getLevel();
 	int getAttack();
 	int getVital();
 	int getDefense();
-	string getSkill(int i); // 포켓몬이 가지고 있는 i 번째 스킬 이름 반환. 
-	string getName();
+	int getFullVital();
+	char * getSkill(int i); // 포켓몬이 가지고 있는 i 번째 스킬 이름 반환. 
+	char * getName();
+
+	int FirstSkill(char skillName[]);
+	int SecondSkill(char skillName[]);
 };
 
 class Pikachu : public Pokemon
 {
 public:
 	Pikachu(int level);
-
-	int FirstSkill(string skillName);
-	int SecondSkill(string skillName);
-
 };
 
 class GuGu : public Pokemon
 {
 public:
 	GuGu(int level);
-
-	int FirstSkill(string skillName);
-	int SecondSkill(string skillName);
 };
+
+class Aboke : public Pokemon
+{
+public:
+	Aboke(int level);
+};
+
+class NyaongE : public Pokemon
+{
+public:
+	NyaongE(int level);
+};
+
+class Ddogas: public Pokemon
+{
+public:
+	Ddogas(int level);
+};
+
+class JilpeokE : public Pokemon
+{
+public:
+	JilpeokE(int level);
+};
+
+class Coil : public Pokemon
+{
+public:
+	Coil(int level);
+};
+
+class Digda : public Pokemon
+{
+public:
+	Digda(int level);
+
+};
+
+class Tanguri : public Pokemon
+{
+public:
+	Tanguri(int level);
+};
+
+
+class Phantom : public Pokemon
+{
+public:
+	Phantom(int level);
+};
+
+class Yungela : public Pokemon
+{
+public:
+	Yungela(int level);
+};
+
+class Parasect : public Pokemon
+{
+public:
+	Parasect(int level);
+};
+
+class Dandangi : public Pokemon
+{
+public:
+	Dandangi(int level);
+};
+
+class Majayong : public Pokemon
+{
+public:
+	Majayong(int level);
+};
+
+class Ddodogas : public Pokemon
+{
+public:
+	Ddodogas(int level);
+};
+
+class Dactrio : public Pokemon
+{
+public:
+	Dactrio(int level);
+
+};
+
+
+class Yadon : public Pokemon
+{
+public:
+	Yadon(int level);
+};
+
+class Yadoran : public Pokemon
+{
+public:
+	Yadoran(int level);
+};
+
+
 
 #endif
